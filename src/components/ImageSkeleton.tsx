927951355b7291321265ce958a214976c382777c
Thomas42061 <70294084+Thomas42061@users.noreply.github.com>
2024-11-10 06:12:11 -0800

    Delete src directory

--- src/components/ImageSkeleton.tsx
+++
@@ -1,7 +0,0 @@
-const ImageSkeleton = () => {
-    return (
-        <div className="animate-pulse rounded-3xl bg-gray-200 h-64 w-full"></div>
-    );
-};
-
-export default ImageSkeleton;
--- src/components/TextSkeleton.tsx
+++
@@ -1,6 +0,0 @@
-const TextSkeleton = () => {
-    return(
-        <div className="animate-pulse mt-1 rounded-xl bg-gray-200 h-6 w-full rounded-xl"></div>
-    )
-}
-export default TextSkeleton;
